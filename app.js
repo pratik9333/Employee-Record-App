56abb4466da23ceee0a05ca7e327e92d8f5a04ef
pratkaswani <pratkaswani9333@gmail.com>
2022-05-01 15:46:16 +0530

    created file structure

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+//cookies and file middleware
+app.use(cookieParser());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+//import all routes here
+const user = require("./api/user.api");
+
+//router middleware
+app.use("/api/v1", user);
+
+module.exports = app;
